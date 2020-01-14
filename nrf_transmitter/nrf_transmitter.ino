00f014074f086e150ff1ceaf1daf52fc82410f39
Dario Tecchia <dariotecchia@gmail.com>
2020-01-14 14:08:49 +0100

    formatting code

--- nrf_transmitter/nrf_transmitter.ino
+++ nrf_transmitter/nrf_transmitter.ino
@@ -1,7 +1,11 @@
 #include <Arduino.h>
+
 #include <SPI.h>
+
 #include <RH_NRF24.h>
+
 #include <DHT.h>
+
 #include <EEPROM.h>
 
 // Singleton instance of the radio driver
@@ -16,59 +20,54 @@ int deviceID = EEPROM.read(0);
 int counter = 0;
 int analogValue = 0;
 
-void setup()
-{
-    Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
 
-    Serial.println("Setup");
+  Serial.println("Setup");
 
-    while (!Serial);
-    
-    if (!nrf24.init())
-    {
-        Serial.println("init failed");
-    }
-    // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
-    if (!nrf24.setChannel(3))
-    {
-        Serial.println("setChannel failed");
-    }
-    if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
-    {
-        Serial.println("setRF failed");
-    }
+  while (!Serial);
 
-    Serial.println("Transmitter started");
+  if (!nrf24.init()) {
+    Serial.println("init failed");
+  }
+  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  if (!nrf24.setChannel(3)) {
+    Serial.println("setChannel failed");
+  }
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm)) {
+    Serial.println("setRF failed");
+  }
+
+  Serial.println("Transmitter started");
 }
 
 // This wrapper is in charge of calling
 // mus be defined like this for the lib work
-void loop()
-{
-    Serial.println("Sending to gateway");
-    analogValue = analogRead(A0);
-    uint8_t data[4];
-    data[0] = deviceID;
-    data[1] = dht.readTemperature();
-    data[2] = dht.readHumidity();
-    data[3] = analogValue;
+void loop() {
+  Serial.println("Sending to gateway");
+  analogValue = analogRead(A0);
+  uint8_t data[4];
+  data[0] = deviceID;
+  data[1] = dht.readTemperature();
+  data[2] = dht.readHumidity();
+  data[3] = analogValue;
 
-    Serial.println("Device ID");
-    Serial.println(data[0]);
+  Serial.println("Device ID");
+  Serial.println(data[0]);
 
-    Serial.println("Temperature");
-    Serial.println(data[1]);
+  Serial.println("Temperature");
+  Serial.println(data[1]);
 
-    Serial.println("Humidity");
-    Serial.println(data[2]);
-    
-    Serial.println("Humidity");
-    Serial.println(data[3]);
-    
-    nrf24.send(data, sizeof(data));
-    nrf24.waitPacketSent();
-    // Now wait for a reply
-    uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-    delay(2000);
+  Serial.println("Humidity");
+  Serial.println(data[2]);
+
+  Serial.println("Humidity");
+  Serial.println(data[3]);
+
+  nrf24.send(data, sizeof(data));
+  nrf24.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  delay(2000);
 }
